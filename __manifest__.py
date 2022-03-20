aff5521a430f18330e74950de6525da75f2d4a04
Youcef Messelem <messelemy@gmail.com>
2022-03-20 13:45:07 +0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': -105,
+    'summary': 'Hospital Management System',
+    'description': """
+This module contains all the required tools for a successful sale.
+    """,
+    'depends': [],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
