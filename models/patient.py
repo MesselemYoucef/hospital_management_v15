0fcd3a001ce5ba31211c7cb9a736a19b9d4120d1
Youcef Messelem <messelemy@gmail.com>
2022-04-05 14:11:28 +0400

    Filters, GroupBy, Archive

--- models/patient.py
+++ models/patient.py
@@ -6,9 +6,11 @@ class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
 
-    name = fields.Char(string="Patient Name")
+    name = fields.Char(string="Name")
+    ref = fields.Char(string="Reference")
     age = fields.Integer(string="Age")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender")
     weight = fields.Float(string="Weight")
     height = fields.Float(string="Height")
+    active = fields.Boolean(string="Active", default=True)
     
