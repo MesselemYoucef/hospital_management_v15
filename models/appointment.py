ca949fc3ae8dc9766f8f7b237dee5716011c6b19
MesselemYoucef <messelemy@gmail.com>
2022-06-14 23:07:23 +0400

    Added a computed field as well as hot to create default values

--- models/appointment.py
+++ models/appointment.py
@@ -10,7 +10,9 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
 
     patient_id = fields.Many2one('hospital.patient', string="Patient")
-    appointment_time = fields.Datetime(string="Appointment Time")
-    booking_date = fields.Date(string="Booking Date")
+    appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
+    booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
+
+    gender = fields.Selection(String="Gender", related="patient_id.gender")
 
     
--- models/patient.py
+++ models/patient.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
+from datetime import date
 
 
 class HospitalPatient(models.Model):
@@ -8,10 +8,23 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char(string="Name", tracking=True)
-    ref = fields.Char(string="Reference")
-    age = fields.Integer(string="Age")
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender")
+    ref = fields.Char(string="Reference", default="Odoo Mates")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender",
+                              default="female")
+
     weight = fields.Float(string="Weight")
     height = fields.Float(string="Height")
     active = fields.Boolean(string="Active", default=True)
-    
+    date_of_birth = fields.Date(String="DOB")
+    age = fields.Integer(string="Age", compute="_compute_age")
+
+    @api.depends("date_of_birth")
+    def _compute_age(self):
+        today = date.today()
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
+
+
