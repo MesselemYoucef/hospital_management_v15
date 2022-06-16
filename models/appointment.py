c10a082c57b9b683eddf6615a90c581675bb03ae
MesselemYoucef <messelemy@gmail.com>
2022-06-16 23:12:07 +0400

    Added a notebook - onchange function - _rec_name field

--- models/appointment.py
+++ models/appointment.py
@@ -8,11 +8,17 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
+    _rec_name = 'ref'
 
     patient_id = fields.Many2one('hospital.patient', string="Patient")
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
+    ref = fields.Char(string="Reference", default="Odoo Mates")
 
     gender = fields.Selection(String="Gender", related="patient_id.gender")
 
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        for rec in self:
+            rec.ref = rec.patient_id.ref
     
