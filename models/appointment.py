1890f2052a8d18bcd9f6eb56c1413c1808e83829
MesselemYoucef <messelemy@gmail.com>
2022-08-15 23:01:27 +0400

    added help message to the buttons in the appointment form

--- models/appointment.py
+++ models/appointment.py
@@ -13,7 +13,7 @@ class HospitalAppointment(models.Model):
     patient_id = fields.Many2one('hospital.patient', string="Patient")
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
-    ref = fields.Char(string="Reference", default="Odoo Mates")
+    ref = fields.Char(string="Reference", default="Odoo Mates" , help="This is the reference")
     prescription = fields.Html(String="Prescription")
     priority = fields.Selection([
         ('0', 'very low'),
